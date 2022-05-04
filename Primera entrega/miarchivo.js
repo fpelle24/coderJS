8c824e5e5ef183734bcd79b82a40971f753538ca
fpelle24 <88352276+fpelle24@users.noreply.github.com>
2022-05-04 18:57:14 -0300

    Add files via upload
    Primer entrega de coder

---
+++ Primera entrega/miarchivo.js
@@ -0,0 +1,67 @@
+let pesos
+let cambioDeMoneda
+let operacionesDiarias = []
+
+const monedas =[
+    { 
+        id: 1,
+        nombre: "Euros",
+        tasaDeCambio: 47.66
+    },
+    {
+        id: 2,
+        nombre: "Dólares",
+        tasaDeCambio: 42.55
+    },
+    {
+        id: 3,
+        nombre: "Pesos Argentinos",
+        tasaDeCambio: 0.37,
+    },
+    {
+        id: 4,
+        nombre: "Libras",
+        tasaDeCambio: 57.95
+    }
+]
+
+let tablaCotizaciones = document.getElementById("Moneda")
+
+monedas.forEach(mon => {
+
+    tablaCotizaciones.innerHTML += `
+    <tr>                
+    <td id="${mon.id}">${mon.nombre} </td>
+    <td>${mon.tasaDeCambio} </td>
+    </tr>
+    </br>
+    `
+})
+
+
+
+function calcular(idMoneda){
+    
+    let moneda = monedas.find(mon => mon.id == idMoneda)
+    let resultado = pesos / moneda.tasaDeCambio
+    return resultado.toFixed(2)
+}
+
+
+
+do {
+
+    pesos = parseFloat(prompt("Ingrese Pesos"));
+
+    cambioDeMoneda = prompt("Ingrese el número de la moneda a la cual desea cambiar\n 1 - Euros\n 2 - Dólares\n 3 - Pesos Argentinos\n 4 - Libras").toLowerCase();
+
+    if (isNaN(pesos) || isNaN(cambioDeMoneda)) {
+        alert("Ingrese ambas opciones en número")
+
+    }  
+
+} while (isNaN(pesos) || isNaN(cambioDeMoneda))
+
+
+document.write(calcular(parseInt(cambioDeMoneda)))
+
